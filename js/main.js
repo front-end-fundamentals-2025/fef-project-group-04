60eb1bddc2a69129dac69932ab8923340a3fb153
Natasha <rike23fw@student.ju.se>
2025-03-07 10:44:42 +0100

    Header image + prices

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 const topButton = document.getElementById('back-to-top');
+const cartButtonAdd = document.getElementById('addToCart');
+const cartButtonRemove = document.getElementById('removeFromCart');
+const cartItemsContainer = document.getElementById('cartItems');
 
+
+/*Scroll up button*/
 window.addEventListener('scroll', () => {
     if (window.scrollY > 300) {
         topButton.style.display = 'block';
@@ -12,6 +17,12 @@ topButton.addEventListener('click', () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 });
 
+/*redirects to products.html button*/
 document.getElementById("view-products").addEventListener("click", function() {
     window.location.href = "products.html";
 });
+
+/*Add item button*/
+cartButtonAdd.addEventListener('click', () => {
+});
+
